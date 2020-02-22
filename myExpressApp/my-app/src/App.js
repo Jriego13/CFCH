28d8f660c54f4f8685bc55314618bce4e9f26f69
Jonathan Riego <Jonathan.riego@ufl.edu>
2020-02-22 17:36:06 -0500

    Added corner logo top left, centered title and set background color

--- myExpressApp/my-app/src/App.js
+++ myExpressApp/my-app/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
+import FluidImage from './FluidImage';
 import logo from './logo.svg';
 import './App.css';
+import { Image } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function clicked(e){
+  console.log("suck mike hawk");
 }
 
+function App(props) {
+
+  return(
+    <div className="background">
+      <div className="Image">
+       <FluidImage src="/images/houselogo.png" id="CornerImage"/>
+       <div id="Title"><p><strong>St. Francis House Gainesville</strong></p></div>
+       <div id ="Subtitle"><p><em>-transitions to end homelessness</em> </p></div>
+      </div>
+    </div>
+  );
+}  
+
 export default App;
+
+// return(
+//   <div className="Welcome">
+//       <header className="Header">
+//           <img src="/Users/jonathanriego/Desktop/CFCH/myExpressApp/my-app/src/images/house image.jpg" class="ui fluid image" />            
+//       </header>
+//   </div>
+// );
+// }
+
+// export default App;
---
+++ myExpressApp/my-app/src/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react'
+import './Welcome.css';
+import App from './App';
+
+function Welcome() {
+    return(
+        <div className="Welcome">
+            <header className="Header">
+                <img src="/images/wireframe/image.png" class="ui fluid image" />            
+            </header>
+        </div>
+    )
+
+};
